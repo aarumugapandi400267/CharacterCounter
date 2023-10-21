7811b193b40938df8e9fe6e45bb02ad252e327d9
AARUMUGAPANDI <123652671+aarumugapandi400267@users.noreply.github.com>
2023-10-21 15:07:09 +0530

    Update dynamic.js

--- dynamic/dynamic.js
+++ dynamic/dynamic.js
@@ -1,5 +1,5 @@
 let inputText=document.getElementById("textarea")
 let characterCount=document.getElementById("characterCount")
 inputText.addEventListener("input",()=>{
-    characterCount.textContent=inputText.value.trim(" ").length
+    characterCount.textContent=inputText.value.length
 })
